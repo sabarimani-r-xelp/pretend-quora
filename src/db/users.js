6e910f745eebe34a32e09d4fe2737e49d3dc3cb8
Sabarimani <sabari@xelpmoc.in>
2019-02-11 17:56:28 +0530

    Linter testing

--- src/db/users.js
+++ src/db/users.js
@@ -32,7 +32,6 @@ export class UsersDB {
     const user = await this.getUserById(id, false);
     if (!user) {
       return null;
-      return;
     }
 
     if (await comparePassword(password, user.passwordHash)) {
